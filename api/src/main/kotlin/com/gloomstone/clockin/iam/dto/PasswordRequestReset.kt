021e26483012527fee16a99f3f86867dd377e3dd
Daniel H <d.hoinka@pm.me>
2026-09-03 23:38:39 +0200

    Validate password reset requests

--- api/src/main/kotlin/com/gloomstone/clockin/iam/dto/PasswordRequestReset.kt
+++ api/src/main/kotlin/com/gloomstone/clockin/iam/dto/PasswordRequestReset.kt
@@ -1,7 +1,12 @@
 package com.gloomstone.clockin.iam.dto
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
 data class PasswordRequestReset(
     val username: String,
+    @field:NotBlank @field:Size(min = 8, max = 200)
     val password: String,
+    @field:NotBlank @field:Size(min = 8, max = 200)
     val passwordRepeat: String
 )
