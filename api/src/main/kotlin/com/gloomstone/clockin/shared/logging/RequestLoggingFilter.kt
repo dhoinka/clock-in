2087fd53a399405c999bf0798db722ffabdf7102
Daniel H <d.hoinka@pm.me>
2026-09-21 21:01:03 +0200

    Fix request logging duration test

--- api/src/main/kotlin/com/gloomstone/clockin/shared/logging/RequestLoggingFilter.kt
+++ api/src/main/kotlin/com/gloomstone/clockin/shared/logging/RequestLoggingFilter.kt
@@ -6,7 +6,6 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
-import org.springframework.util.StopWatch
 import org.springframework.web.filter.OncePerRequestFilter
 
 @Component
